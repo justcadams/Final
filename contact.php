e885ccc46f706cdeebb989a0ddb810a805cd9ee6
Eric <Eric@10.245.109.149>
2019-04-25 15:27:04 -0400

    fixed the location thing so now it shows the map and KK

--- contact.php
+++ contact.php
@@ -10,6 +10,18 @@ else {
 ?>
 <article>
 <h1>Kampus Kitche Deli</h1>
-<h2>273 Colchester Avenue<br>BUrlington, VT 05401<br>Phone: (802) 863-9105<br>kampuskitchendeli@gmail.com</h2>
+<h2>273 Colchester Avenue<br>Burlington, VT 05401<br>Phone: (802) 863-9105<br>kampuskitchendeli@gmail.com</h2>
 </article>
+<div id="googleMap" style="width: 100%;height: 500px"></div>
+<script>
+	function myMap(){
+	var mapSize = {
+		center:new google.maps.LatLng(44.483105,-73.190147),
+		zoom:17,
+	};
+	var map = new
+	google.maps.Map(document.getElementById("googleMap"),mapSize);
+}
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBO0XvT7GDVmdD1KNtbVVz5ApbFvZF5EE4&callback=myMap"></script>  
 <?php include 'footer.php';?>
