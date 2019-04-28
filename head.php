13caf624945f011d0d15ffb5976a603191dc7e78
Eric <Eric@10.245.72.193>
2019-04-28 13:37:21 -0400

    changed K to C

--- head.php
+++ head.php
@@ -2,7 +2,7 @@
 	<title>Viridian General Partnership</title>
 		<meta charset="UTF-8">
 		<meta name="author" content="Aviation Deli Group, Inc.">
-		<meta name="business" content="Doing Business As Kampus Kitchen Deli">
+		<meta name="business" content="Doing Business As Campus Kitchen Deli">
 		<meta name="description" content="Homepage Site Map">
 		<link rel="stylesheet" href='css/custom.css'>
 		<link rel="icon" type="image/png" href="images/aviation-logo.jpg">
