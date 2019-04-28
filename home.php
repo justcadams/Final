41b8a476e928065f0c44f603809d7b8730258107
Eric <Eric@10.245.72.193>
2019-04-28 15:22:07 -0400

    changed colors

--- home.php
+++ home.php
@@ -8,12 +8,5 @@ else {
 	include 'top.php';
 }
 ?>
-<article>
-	<h1>Welcome to Campus Kitchen and Aviation Deli!</h1>
-	<figure class="coverpic">
-            <img class="coverimg" src="images/KKStorefront.jpg" alt="Campus Kitchen Storefront">
-                <figcaption>Campus Kitchen Storefront</figcaption>
-            </figure>
 
-</article>
 <?php include 'footer.php';?>
