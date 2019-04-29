c9f0446cfbd47663b826445c9e25b9de144c8894
Eric <Eric@10.245.72.193>
2019-04-28 23:25:58 -0400

    Styling Menu sheets

--- www-root/cs008/footer.php
+++ www-root/cs008/footer.php
@@ -1,6 +1,6 @@
 <!--############################################## Footer ##############################################-->
 		<footer>
-			<h2 class="center footer">Justin Adams & Eric Kloeti, JEL LLC &copy; 24 April 2019</h2>
+			<h2 class="center-footer">Justin Adams & Eric Kloeti, JEL LLC &copy; 24 April 2019</h2>
 		</footer>
 		</main>
 	</body>
