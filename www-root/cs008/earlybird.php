8db4ddf27d37ea8db523a015c72c80c63da10d7e
Eric <Eric@10.245.72.193>
2019-04-28 20:05:57 -0400

    New Menu

---
+++ www-root/cs008/earlybird.php
@@ -0,0 +1,91 @@
+<?php 
+$currentDirectory = getcwd();
+if($currentDirectory != '/users/j/a/jadams7/www-root/cs008/final') {
+	chdir('/users/j/a/jadams7/www-root/cs008/final');
+	require 'top.php';
+}
+else {
+	require 'top.php';
+}
+?>
+<article class="center" id="april">
+<h1>Menu</h1>
+<h2 class="bold">Full catering large and small</h2>
+<h3 class="bold">Call or order online to view the menu and pricing!</h3>
+<h2 class="italic">Ask to join our e-mail list in store or <a href="newsletter.php">sign up</a> on the newsletter page.</h2>
+
+<h3>Breakfast Sandwiches</h3>
+<h4 class="bold italic">The Little John - <p class="inline">$6.99</p></h4>
+<p>Ham, Bacon, Sausage, 2 Eggs, Cheese, Hash Brown, and Sirracha Sauce on a Large Ciabatta.</p>
+<h4 class="bold italic">The Lumberjack - <p class="inline">$4.25</p></h4>
+<p>Egg, American Cheese, Hash Brown, Choice of Meat (Bacon, Ham, or Sausage) on an English Muffin.</p>
+<h4 class="bold italic">The Sassy Chicken Wrap - <p class="inline">$4.49</p></h4>
+Egg, Buffalo Chicken, Jalapeno, Ranch, Sirracha Sauce & Cheddar Cheese.
+<h4 class="bold italic">Western Omelet Wrap - <p class="inline">$4.49</p></h4>
+<p>Black Forest Ham, Onion, Green Pepper, American Cheese, & Egg.</p>
+<h4 class="bold italic">Sirracha Breakfast Wrap - <p class="inline">$4.99</p></h4>
+<p>Sausage, Bacon, Broccoli, Tomato, American Cheese, & Egg.</p>
+<h4 class="bold italic">The JC Veggie Special Breakfast Wrap - <p class="inline">$4.49</p></h4>
+<p>Black Beans, Salsa, Onion, Tomato, American Cheese, & Egg.</p>
+<h4 class="bold italic">Steak Egg & Cheese - <p class="inline">$4.99</p></h4>
+<p>Seasoned Shaved Steak, Cheddar Cheese, American Cheese, Fried Egg on a Ciabatta Roll.</p>
+<h4 class="bold italic">Pastrami Reuben Breakfast Wrap - <p class="inline">$4.49</p></h4>
+<p>Pastrami, Sauerkraut, Swiss Cheese, Thousand Island Dressing.</p>
+<h4 class="bold italic">Make Your Own Sunrise Sandwich</h4>
+<h4 class="bold">Meats: Bacon, Ham, Pastrami, Sausage, Steak, Buffalo Chicken, Egg, Double Egg</h4>
+<h4 class="bold">Cheese: American, Pepper Jack, Provolone, Swiss</h4>
+<h4 class="bold">Veggies: Banana Pepper, Black Beans, Black Olives, Broccoli, Green Pepper, Hash Brown, Jalapeno, Onion, Pickles, Sauerkraut, Tomato</h4>
+<h4 class="bold">Dressing: Ranch, Salsa, Sirracha, Thousand Island Dressing</h4>
+<h4 class="bold">Bread: English Muffin, Butter Croissant, Buttermilk Biscuit, Small Ciabatta, or Large Ciabatta</h4>
+
+<h3>Lunch Specials</h3>
+<h3>Salads</h3>
+<h4 class="bold italic">Garden Salad - <p class="inline">$5.95</p></h4>
+<p>(Fresh Greens, Tomatoes, Onions, Peppers, Cucumbers, Carrots).</p>
+<h4 class="bold italic">Chef Salad - <p class="inline">$7.95</p></h4>
+<p>(Garden Salad with Ham, Turkey, Egg, and Swiss Cheese).</p>
+<h4 class="bold italic">Buffalo Chicken Caeser - <p class="inline">$7.95</p></h4>
+<p>(Fresh Greens, Parmesan Cheese, Breaded Buffalo Chicken, Bacon)</p>
+
+<h3>Soups</h3>
+<p>Made fresh daily!<br>
+Call to find out our specials.</p>
+
+<h3>Subs &amp; Sandwiches</h3>
+<h5 class="twocolcenter bold italic"><p class="col">Small $5.99</p><p class="col">Large $8.99</p></h5>
+<h4>Rolls: <p class="inline">Sub, White or Wheat. Ciabatta, White or Wheatberry.</p></h4>
+<h4>Wraps: <p class="inline">White, Wheat, Spinach, Sundried Tomato, Jalapeno Cheddar, or Garlic Herb.</p></h4>
+<h4>Meats: <p class="inline">Ham, Turkey, Roast Beef, Salami, Capicola, Chicken Salad, Tuna Salad, and Seafood Salad.</p></h4>
+<h4>Toppings: <p class="inline">Swiss, American, Provolone, Cheddar, Romaine Lettuce, Tomato, Sliced Red Onions, Pickles, Banana Peppers, Green Peppers, Black Olives, Jalapenos, Cucumber, Carrot, Honey Dijon, Deli Mustard, Mayonnaise, Oil Blend, Chipotle Mayonnaise, Horseradish Mayonnaise, Sirracha Mayonnaise, Garlic Mayonnaise.</p></h4>
+
+<h3>Specialty Sandwiches</h3>
+<h5 class="twocolcenter bold italic"><p class="col">Small $5.99</p><p class="col">Large $8.99</p></h5>
+<h4 class="bold inline">Italian - </h4><p class="inline"><br>Ham, Salami, Capicola, Provolone Cheese, Italian Dressing, and your choice of vegetables.</p><br>
+<h4 class="bold inline">BLT - </h4><p class="inline"><br>Bacon, Lettuce, Tomato, Mayonnaise.</p><br>
+<h4 class="bold inline">Hot Pastrami - </h4><p class="inline"><br>Mustard, Swiss Cheese, Lean Pastrami Heated to Perfection!</p><br>
+
+<h3>Veggie Sandwich</h3>
+<h5 class="twocolcenter bold italic"><p class="col">Small $4.99</p><p class="col">Large $7.99</p></h5>
+<h4 class="bold inline">Vegetable - </h4><p class="inline">Cheese and your choice of vegetables.</p></h4>
+
+<h3 class="bold">Paninis<p class="inline">$8.99</p></h3>
+<h4 class="bold">Meat Loaf Panini Wrap</h4>
+<p class="inline">Meat Loaf, Bacon, Mustard, Ketchup, American Cheese.</p>
+<h4 class="bold">Salami Panini</h4>
+<p class="inline">Genoa Hard Salami, Fresh mozzarella Cheese, Tomato, Garlic, Sauteed Spinach, Basil Pesto Aioli on Ciabatta Bread.</p>
+<h4 class="bold">The Cuban</h4>
+<p class="inline">Provolone Cheese, Ham, Pulled Pork, Pickles, Yellow Mustard.</p>
+<h4 class="bold">Roast Beef</h4>
+<p class="inline">Roast Beef, Horseradish Mayonnaise, Roasted Red Peppers, Cabot Cheddar Cheese.</p>
+<h4 class="bold">Turkey Bacon</h4>
+<p class="inline">Turkey, Bacon, Cheddar Cheese, Tomato, Chipotle Mayonnaise.</p>
+<h4 class="bold">Rachel</h4>
+<p class="inline">Turkey, Sauerkraut, Swiss Cheese, Thousand Island Dressing.</p>
+<h4 class="bold">Passionate Pig <p class="inline">+$1.00</p></h4>
+<p class="inline">Pulled pork, Black Forest Ham, Jalapeno Bacon, Smoked Gouda, Fire Roasted Jalapeno Garlic Aioli.</p>
+<h4 class="bold">Basil Pesto Panini</h4>
+<p class="inline">Fresh Mozzarella, Tomato, Baby Spinach, Basil Pest on Panini Flat Bread.</p>
+<h4 class="bold">Tasty Italian Panini</h4>
+<p class="inline">McKenzie Black Forest Ham, Genoa Salami, Hot Capicola, Provolone Cheese, Red Onion, Banana Pepper, Black Olive, House Made Italian Oil, and Mayonnaise on a Sub Roll.</p>
+</article>
+<?php require 'footer.php';?>
