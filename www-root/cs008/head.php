a7a9e0caf5116242b11da95900f4bab323816513
Justin Adams <justin.adams@uvm.edu>
2019-04-28 20:58:04 -0400

    removed css link from head.php

--- www-root/cs008/head.php
+++ www-root/cs008/head.php
@@ -4,7 +4,6 @@
 		<meta name="author" content="Aviation Deli Group, Inc.">
 		<meta name="business" content="Doing Business As Campus Kitchen Deli">
 		<meta name="description" content="Homepage Site Map">
-		<link rel="stylesheet" href='css/custom.css'>
 		<link rel="icon" type="image/png" href="images/aviation-logo.jpg">
 		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Oswald">
-                <link rel="stylesheet" type="text/css" href="//fonts.gooleapis.com/css?family=Spectral">
+        <link rel="stylesheet" type="text/css" href="//fonts.gooleapis.com/css?family=Spectral">
