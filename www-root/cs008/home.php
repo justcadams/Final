1fdabd8caa9a6335c639b176378c981fe90bad0c
Justin Adams <justin.adams@uvm.edu>
2019-05-01 18:33:57 -0400

    Added campus kitchen picture.

--- www-root/cs008/home.php
+++ www-root/cs008/home.php
@@ -8,5 +8,8 @@ else {
 	include 'top.php';
 }
 ?>
-
+<figure class="logo shadow float right">
+    <img alt="Deli Duo" src="images/campus-kitchen.jpg" class="logo">
+	<figcaption class="center">Campus Kitchen serves fresh food with a smile. Swing by for our daily specials. We update our menu weekly, and serve McKenzie meats, cheeses, and fresh deli produce. Visit Campus Kitchen on campus today to enjoy Vermont's best locally sourced products.</figcaption>
+</figure>
 <?php include 'footer.php';?>
